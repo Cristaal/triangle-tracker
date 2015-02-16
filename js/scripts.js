0765a002264344c5ddd15837462f7459b50677bc
Crystal Contreras-Grossman and Karvari Ellingson <crystalcontrerasgrossman@gmail.com>
2015-02-16 11:05:19 -0800

    Add ability to add and list names

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,25 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedAddress = $("input#new-address").val();
-    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
+    var newContact = { firstName: inputtedFirstName,
+                       lastName: inputtedLastName,
+                       address: inputtedAddress };
 
-    $("ul#contact").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" +
+                           newContact.firstName + " " +
+                           newContact.lastName +
+                           "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-address").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName + "" + newContact.lastName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".address").text(newContact.address);
+    });
   });
-});
-
-
-$(".contact").last().click(funtion() {
-  $("#show-contact").show();
-  $("#show-contact h2").text(newContact.firstName + "" + newContact.lastName);
-  $(".first-name").text(newContact.firstName);
-  $(".last-name").text(newContact.lastName);
-  $(".address").text(newContact.address);
 });
