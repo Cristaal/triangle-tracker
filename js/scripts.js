cabc8156a0effb0e4822e20b75f76d1a028e35bb
Crystal Contreras-Grossman and Monty Rohde <crystalcontrerasgrossman@gmail.com>
2015-02-17 11:42:46 -0800

    add initial files

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,30 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedAddress = $("input#new-address").val();
+    var inputtedStreet1 = $("input#new-street1").val();
+    var inputtedStreet2 = $("input#new-street2").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+    var inputtedZip = $("input#new-zip").val()
+
+    var newAddress = { street1: inputtedStreet1,
+                       street2: inputtedStreet2,
+                       city: inputtedCity,
+                       state: inputtedState,
+                       zip: inputtedZip,
+                       fullAddress: function() {
+                         if ((/([\w])/).test(this.street2)) {
+                           return this.street1 + ', ' + this.street2 + ', ' + this.city + ', ' + this.state + ', ' + this.zip;
+                         } else {
+                           return this.street1 + ', ' + this.city + ', ' + this.state + ', ' + this.zip;
+                         }
+                       }
+                      };
+
     var newContact = { firstName: inputtedFirstName,
                        lastName: inputtedLastName,
-                       address: inputtedAddress };
+                       address: newAddress.fullAddress()
+                     };
 
     $("ul#contacts").append("<li><span class='contact'>" +
                            newContact.firstName + " " +
@@ -16,13 +36,15 @@ $(document).ready(function() {
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-    $("input#new-address").val("");
+    $("input#new-street1").val("");
+    $("input#new-street2").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+    $("input#new-zip").val("");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName + "" + newContact.lastName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
       $(".address").text(newContact.address);
     });
   });
